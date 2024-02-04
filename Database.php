c76371d4dbc313dc87025316bf469ee459634242
Жиганов Тимофей Александрович <zhiganov_t@tass.ru>
2024-02-05 02:17:51 +0300

    Разнес обработку спецификаторов по отдельным функциям

--- Database.php
+++ Database.php
@@ -19,65 +19,19 @@ class Database implements DatabaseInterface
         $newQuery = "";
         $argsKey = 0;
         $query = mb_str_split($query);
-
         for ($i = 0; $i < count($query); $i++) {
             if ($query[$i] == '?') {
                 $param = $args[$argsKey];
-                if (!isset($query[$i + 1]) || (isset($query[$i + 1]) && $query[$i + 1] != 'a' && $query[$i + 1] != '#' && $query[$i + 1] != 'd' && $query[$i + 1] != 'f')) {
-                    if (!is_string($param) && !is_bool($param) && !is_int($param) && !is_float($param) && !is_null($param)) {
-                        throw new Exception('Неверный тип спецификатора');
-                    }
-                }
-                if ($args[$argsKey] === null && (!isset($query[$i + 1]) || (isset($query[$i + 1]) && $query[$i + 1] != 'a' && $query[$i + 1] != '#'))) {
-                    $param = 'NULL';
-                } elseif (isset($query[$i + 1])) {
-                    if ($query[$i + 1] != 'a' && $query[$i + 1] != '#' && $query[$i + 1] != 'd' && $query[$i + 1] != 'f') {
-                        $param = $this->convertParam($args[$argsKey]);
-                    } else {
-                        if ($query[$i + 1] == 'd') {
-                            $param = intval($param);
-                        } elseif ($query[$i + 1] == 'f') {
-                            $param = floatval($param);
-                        } elseif ($query[$i + 1] == '#' && is_array($args[$argsKey])) {
-                            $param = "";
-                            foreach ($args[$argsKey] as $key => $value) {
-                                if (is_string($value)) {
-                                    $param .= '`' . $value . '`';
-                                } else {
-                                    $param .= $value;
-                                }
-                                if ($key != (count($args[$argsKey]) - 1)) {
-                                    $param .= ', ';
-                                }
-                            }
-                        } elseif ($query[$i + 1] == '#' && is_string($args[$argsKey])) {
-                            $param = '`' . $param . '`';
-                        } elseif ($query[$i + 1] == 'a' && is_array($args[$argsKey])) {
-                            $param = "";
-                            if (array_is_list($args[$argsKey])) {
-                                foreach ($args[$argsKey] as $key => $value) {
-                                    $param .= $value;
-                                    if ($key != (count($args[$argsKey]) - 1)) {
-                                        $param .= ', ';
-                                    }
-                                }
-                            } else {
-                                $n = 0;
-                                foreach ($args[$argsKey] as $key => $value) {
-                                    $n++;
-                                    $param .= '`' . $key . '` = ' . $this->convertParam($value);
-                                    if ($n != (count($args[$argsKey]))) {
-                                        $param .= ', ';
-                                    }
-
-                                }
-                            }
-                        }
-                        $i++;
-                        $argsKey++;
-                    }
-                } else {
-                    $param = $this->convertParam($args[$argsKey]);
+                $this->checkQuerySpecifierByIterator($query, $i, $param);
+                $param = $this->convertQueryParamByIterator($query, $i, $args[$argsKey]);
+                $param = $this->convertSpecifierDByIterator($query, $i, $param);
+                $param = $this->convertSpecifierFByIterator($query, $i, $param);
+                $param = $this->convertSpecifierPoundByIterator($query, $i, $param);
+                $param = $this->convertSpecifierAByIterator($query, $i, $param);
+                $param = $this->convertParamNullByIterator($query, $i, $param);
+                if ($this->checkIsSpecifierPlusByIterator($query, $i)) {
+                    $i++;
+                    $argsKey++;
                 }
                 $newQuery .= $param;
             } elseif ($query[$i] == '{') {
@@ -97,6 +51,109 @@ class Database implements DatabaseInterface
         }
 
         return $newQuery;
+    }
+
+
+    private function checkQuerySpecifierByIterator(array $query, int $i, $param)
+    {
+        if (!isset($query[$i + 1]) || (isset($query[$i + 1]) && $query[$i + 1] != 'a' && $query[$i + 1] != '#' && $query[$i + 1] != 'd' && $query[$i + 1] != 'f')) {
+            if (!is_string($param) && !is_bool($param) && !is_int($param) && !is_float($param) && !is_null($param)) {
+                throw new Exception('Неверный тип спецификатора');
+            }
+        }
+    }
+
+    private function convertParamNullByIterator(array $query, int $i, $param)
+    {
+        if ($param === null && (!isset($query[$i + 1]) || (isset($query[$i + 1]) && $query[$i + 1] != 'a' && $query[$i + 1] != '#'))) {
+            return 'NULL';
+        }
+        return $param;
+    }
+
+
+    private function convertQueryParamByIterator(array $query, int $i, $param)
+    {
+        if (isset($query[$i + 1]) && $query[$i + 1] != 'a' && $query[$i + 1] != '#' && $query[$i + 1] != 'd' && $query[$i + 1] != 'f') {
+            return $this->convertParam($param);
+        }
+        return $param;
+    }
+
+    private function checkIsSpecifierPlusByIterator(array $query, int $i): bool
+    {
+        if (isset($query[$i + 1]) && ($query[$i + 1] == 'a' || $query[$i + 1] == '#' || $query[$i + 1] == 'd' || $query[$i + 1] == 'f')) {
+            return true;
+        }
+        return false;
+    }
+
+    private function convertSpecifierDByIterator(array $query, int $i, $param)
+    {
+        if (isset($query[$i + 1]) && $query[$i + 1] == 'd') {
+            $param = intval($param);
+        }
+        return $param;
+    }
+
+    private function convertSpecifierFByIterator(array $query, int $i, $param)
+    {
+        if (isset($query[$i + 1]) && $query[$i + 1] == 'f') {
+            $param = floatval($param);
+        }
+        return $param;
+    }
+
+    private function convertSpecifierPoundByIterator(array $query, int $i, $param)
+    {
+        if (!isset($query[$i + 1]) || $query[$i + 1] != '#') {
+            return $param;
+        }
+        if (is_array($param)) {
+            $paramHelp = "";
+            foreach ($param as $key => $value) {
+                if (is_string($value)) {
+                    $paramHelp .= '`' . $value . '`';
+                } else {
+                    $paramHelp .= $value;
+                }
+                if ($key != (count($param) - 1)) {
+                    $paramHelp .= ', ';
+                }
+            }
+            return $paramHelp;
+        }
+        if (is_string($param)) {
+            $param = '`' . $param . '`';
+        }
+        return $param;
+    }
+
+    private function convertSpecifierAByIterator(array $query, int $i, $param)
+    {
+        if (isset($query[$i + 1]) && $query[$i + 1] == 'a' && is_array($param)) {
+            $paramHelp = "";
+            if (array_is_list($param)) {
+                foreach ($param as $key => $value) {
+                    $paramHelp .= $value;
+                    if ($key != (count($param) - 1)) {
+                        $paramHelp .= ', ';
+                    }
+                }
+            } else {
+                $n = 0;
+                foreach ($param as $key => $value) {
+                    $n++;
+                    $paramHelp .= '`' . $key . '` = ' . $this->convertParam($value);
+                    if ($n != (count($param))) {
+                        $paramHelp .= ', ';
+                    }
+
+                }
+            }
+            return $paramHelp;
+        }
+        return $param;
     }
 
     private function convertParam($param)
